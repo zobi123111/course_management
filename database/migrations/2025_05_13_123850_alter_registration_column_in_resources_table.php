5031b7bc5ecd2017f71f131819de284b9a1b8d62
vikasthakurC4E <vikas@code4each.com>
2025-05-13 18:10:41 +0530

    migration for resource table

---
+++ database/migrations/2025_05_13_123850_alter_registration_column_in_resources_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            $table->string('registration')->nullable()->default(null)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('resources', function (Blueprint $table) {
+            $table->string('registration')->nullable(false)->default('')->change();
+        });
+    }
+};
