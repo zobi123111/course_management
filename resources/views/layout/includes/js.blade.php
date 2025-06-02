cf6afc4d372468273db718a2e6b3aa6f174f5bb2
vikasthakurC4E <vikas@code4each.com>
2025-06-02 17:44:20 +0530

    implemented Total time in training event and fixed bug

--- resources/views/layout/includes/js.blade.php
+++ resources/views/layout/includes/js.blade.php
@@ -15,7 +15,7 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
 <script>
 tinymce.init({
     selector: 'textarea#myeditorinstance', // Replace this CSS selector to match the placeholder element for TinyMCE
