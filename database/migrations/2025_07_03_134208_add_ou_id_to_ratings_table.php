a57e7c437d05506f2670bca6ddf1d4561ffc6704
nitin-0444 <nitin@code4each.com>
2025-07-07 13:43:23 +0530

    add ou_id

--- database/migrations/2025_07_03_134208_add_ou_id_to_ratings_table.php
+++ database/migrations/2025_07_03_134208_add_ou_id_to_ratings_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('parent_rating', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('rating_id');
-            $table->unsignedBigInteger('parent_id');
-            $table->timestamps();
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->unsignedBigInteger('ou_id')->nullable()->after('id');
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('parent_rating');
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->dropColumn('ou_id');
+        });
     }
 };
